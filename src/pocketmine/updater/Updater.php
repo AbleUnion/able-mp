5c4c5b6924a36d8df0b5b2740ea1327aba56422e
djdisodo <twanny@DESKTOP-Q5VKN78>
2017-09-04 16:30:20 +0900

    자동 업데이터

--- src/pocketmine/updater/Updater.php
+++ src/pocketmine/updater/Updater.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  *   ____  _            _      _       _     _
  *  |  _ \| |          | |    (_)     | |   | |
@@ -17,154 +16,72 @@
  * @author BlueLightJapan Team
  * 
 */
- 
 namespace pocketmine\updater{
-	
-	echo "\x1b]0;BlueLight-Updater\x07";
-
-	echo getLogo() . PHP_EOL;
-	echo "\x1b[mLoading branches..." . PHP_EOL;
-	
-	$branches = json_decode(getURL("https://api.github.com/repos/BlueLightJapan/BlueLight/branches"), true);
-	
-	echo "\x1b[mBranch Name: ";
-	
-	$array = [];
-	$count = count($branches);
-	
-	foreach($branches as $b){
-		$count--;
-		$array[] = $b["name"];
-		if($count == 0){
-			echo "\x1b[38;5;83m".$b["name"] . PHP_EOL;
-		}else{
-			echo "\x1b[38;5;83m".$b["name"].", ";
-		}
-	}
-	
-	do{
-		echo "\x1b[mSelect Branch: \x1b[38;5;87m";
-		$branch = trim(fgets(STDIN));
-		if($tmp = !in_array($branch, $array)){
-			echo "\x1b[38;5;124mThe branch is not found." . PHP_EOL;
-		}
-	}while($tmp);
-	
-	if(!file_exists("temp")) mkdir("temp");
-	
-	echo "\x1b[mDownload BlueLight-".$branch." now..." . PHP_EOL;
-	
-	$data = @file_get_contents("https://github.com/BlueLightJapan/BlueLight/archive/".$branch.".zip");
-	if($data){
-		$result = file_put_contents("temp/BlueLight.zip",$data);
-		echo "\x1b[38;5;83mDownload Success!" . PHP_EOL;
-	}else{
-		echo "\x1b[38;5;124mDownload Failed." . PHP_EOL;
-		dirrm("temp");
-		exit(1);
-	}
-	
-	echo "\x1b[mExtract Zip File..." . PHP_EOL;
-	$zip = extractZip("temp/BlueLight.zip");
-
-	if($zip){
-		echo "\x1b[38;5;83mUnZip Success!" . PHP_EOL;
-	}else{
-		echo "\x1b[38;5;124mUnZip Failed" . PHP_EOL;
-		dirrm("temp");
-		exit(1);
-	}
-	
-	if(file_exists("PocketMine-MP.phar")) unlink("PocketMine-MP.phar");
-	if(file_exists("src")) dirrm("src");
-	
-	dircopy("temp/BlueLight-".$branch."/src","src");
-	dirrm("temp");
-	echo "\x1b[38;5;83mCompleted BlueLight Update!\x1b[m" . PHP_EOL;
-	
-	exit(1);
-
-
-	function getLogo(){
-		$logo = "\x1b[38;5;87m				 ____  _            _      _       _     _
-				|  _ \| |          | |    (_)     | |   | |
-				| |_) | |_   _  ___| |     _  __ _| |__ | |_
-				|  _ <| | | | |/ _ \ |    | |/ _` | '_ \| __|
-				| |_) | | |_| |  __/ |____| | (_| | | | | |_
-				|____/|_|\__,_|\___|______|_|\__, |_| |_|\__|
-							      __/ |
-							     |___/
-				\x1b[m";
-		return $logo;
-	}
-	
-	function getURL($url, $timeout = 10){
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array("User-Agent: Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36"));
-		curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-		curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
-		curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, (int) $timeout);
-		curl_setopt($ch, CURLOPT_TIMEOUT, (int) $timeout);
-		$result = curl_exec($ch);
-		$err = curl_error($ch);
-		curl_close($ch);
-
-		return $result;
-	}
-	
-	function extractZip($path){
+	function extractZip($path, $file){
 		$zip = new \ZipArchive();
-		$res = $zip->open($path);
+		$res = $zip->open($file);
 		if ($res == true){
-			$zip->extractTo("temp");
+			$zip->extractTo($path);
 			$zip->close();
 			return true;
 		}else{
 			return false;
 		}
 	}
-	
-	function dirrm($dir) {
-		if($handle = opendir($dir)) {
-			while (false !== ($item = readdir($handle))) {
-				if ($item != "." && $item != "..") {
-					if(is_dir($dir."/".$item)) {
-						dirrm($dir."/".$item);
-					}else{
-						unlink($dir."/".$item);
-					}
-				}
-			}
-			closedir($handle);
-			rmdir($dir);
-		}
+	function rmdir_ok($dir) {
+		     $dirs = dir($dir);
+		     while(false !== ($entry == $dirs->read())) {
+			         if(($entry != '.') && ($entry != '..')) {
+				             if(is_dir($dir.'/'.$entry)) {
+					                   rmdir_ok($dir.'/'.$entry);
+					             } else {
+						                   @unlink($dir.'/'.$entry);
+						             }
+						         }
+						     }
+						     $dirs->close();
+						     @rmdir($dir);
+						 }
+						 function fileCopy($odir,$ndir) {
+						 	      if(filetype($odir) === 'dir') {
+						 		           clearstatcache();
+						 
+						 		           if($fp = @opendir($odir)) {
+						 			                  while(false !== ($ftmp = readdir($fp))){
+						 				                        if(($ftmp !== ".") && ($ftmp !== "..") && ($ftmp !== "")) {
+						 					                              if(filetype($odir.'/'.$ftmp) === 'dir') {
+						 						                                   clearstatcache();
+						 						      
+						 						                                   @mkdir($ndir.'/'.$ftmp);
+						 						                                   echo ($ndir.'/'.$ftmp."<br />\n");
+						 						                                   set_time_limit(0);
+						 						                                   fileCopy($odir.'/'.$ftmp,$ndir.'/'.$ftmp);
+						 						                              } else {
+						 							                                   copy($odir.'/'.$ftmp,$ndir.'/'.$ftmp);
+						 							                              }
+						 							                        }
+						 							                  }
+						 							           }
+						 							           if(is_resource($fp)){
+						 								                 closedir($fp);
+						 								           }
+						 								      } else {
+						 									            echo $ndir."<br />\n";     
+						 									            copy($odir,$ndir);
+						 									      }
+						 									 } // end func
+	if(!is_file("buildno.txt")){
+		file_put_contents('buildno.txt', 0);
 	}
-
-	function dircopy($dir_name, $new_dir){
-		if (!is_dir($new_dir)) {
-			mkdir($new_dir, 0777, true);
-		}
- 
-		if (is_dir($dir_name)){
-			if ($dh = opendir($dir_name)) {
-				while (($file = readdir($dh)) !== false) {
-					if ($file == "." || $file == "..") {
-						continue;
-					}
-					if (is_dir($dir_name . "/" . $file)) {
-					dircopy($dir_name . "/" . $file, $new_dir . "/" . $file);
-					}else{	
-						copy($dir_name . "/" . $file, $new_dir . "/" . $file);
-					}
-				}
-				closedir($dh);
-			}
-		}
+	if(file_get_contents("buildno.txt") !== file_get_contents("https://raw.githubusercontent.com/AbleUnion/able-mp/master/buildno.txt")) {
+		echo 'you need to update' . PHP_EOL;
+		mkdir(sys_get_temp_dir() . '/updater/');
+		$temp = sys_get_temp_dir() . '/updater/';
+		echo $temp;
+		file_put_contents($temp . '/files.zip', file_get_contents('https://github.com/AbleUnion/able-mp/archive/master.zip'));
+		extractZip($temp, $temp . '/files.zip');
+		rmdir_ok('src');
+		fileCopy($temp . '/able-mp-master', './');
 	}
 }
 	 
