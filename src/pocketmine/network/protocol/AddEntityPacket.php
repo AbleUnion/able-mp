7df38fbca371d395810595545aec9ba468881a94
jisoohero30@gmail.com <jisoohero30@gmail.com>
2017-10-22 14:01:57 +0000

    ..

---
+++ src/pocketmine/network/protocol/AddEntityPacket.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace pocketmine\network\protocol;
+
+class AddEntityPacket extends pocketmine\network\mcpe\protocol\AddEntityPacket {
+	
+}
---
+++ src/pocketmine/network/protocol/ExplodePacket.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace pocketmine\network\protocol;
+
+class ExplodePacket extends pocketmine\network\mcpe\protocol\ExplodePacket {
+	
+}
